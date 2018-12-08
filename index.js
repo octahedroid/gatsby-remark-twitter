de675c2bcfdcb38cae327822b4224573d7230e4c
Jesus Manuel Olivas <jesus.olivas@gmail.com>
2018-12-08 08:11:10 -0800

    first code (#1)
    * first code
    
    * fix typo
    
    * fix package.json

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+const visit = require('unist-util-visit');
+const fetch = require('node-fetch');
+
+const getBloquequote = async (url) => {
+  try {
+    const apiUrl = `https://publish.twitter.com/oembed?url=${url}`;
+
+    const response = await fetch(apiUrl);
+    const json = await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = async ({ markdownAST }) => {
+  visit(markdownAST, 'text', async (node) => {
+    const { value } = node;
+    const tweetLink = value.match(/https:\/\/twitter\.com\/([A-Za-z0-9-_]*\/status\/[A-Za-z0-9-_?=]*)/gi);
+    if (tweetLink) {
+      console.log(`\n embeding tweet: ${tweetLink} \n`);
+      const embedData = await getBloquequote(tweetLink);
+      node.type = 'html';
+      node.value = embedData.html;
+    }
+  });
+
+  return markdownAST;
+};
