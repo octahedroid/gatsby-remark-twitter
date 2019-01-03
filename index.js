78109cd8e888f51bc68225a9c7478857b04a42b0
isaacs <i@izs.me>
2019-01-03 13:03:40 -0800

    Add debug: true option, turn off output otherwise

--- index.js
+++ index.js
@@ -4,33 +4,37 @@ const visit = require('unist-util-visit');
 const fetch = require('node-fetch');
 
 const getBloquequote = async (url) => {
-  try {
-    const apiUrl = `https://publish.twitter.com/oembed?url=${url}`;
+  const apiUrl = `https://publish.twitter.com/oembed?url=${url}`;
 
-    const response = await fetch(apiUrl);
-    return await response.json();
-  } catch (error) {
-    console.log(error);
-  }
+  const response = await fetch(apiUrl);
+  return await response.json();
 };
 
-module.exports = async ({ markdownAST }) => {
+module.exports = async ({ markdownAST }, pluginOptions) => {
+  const debug = pluginOptions.debug ? console.log : () => {}
+
   const nodes = [];
   visit(markdownAST, 'text', (node) => {
     const { value } = node;
     const tweetLink = value.match(/https:\/\/twitter\.com\/([A-Za-z0-9-_]*\/status\/[A-Za-z0-9-_?=]*)/gi);
     if (tweetLink) {
+      debug(`\nfound tweetLink`, node)
       nodes.push([node, tweetLink]);
     }
   })
+
   for (let i = 0; i < nodes.length; i++) {
     const nt = nodes[i];
     const node = nt[0];
     const tweetLink = nt[1];
-    console.log(`\n embeding tweet: ${tweetLink} \n`);
-    const embedData = await getBloquequote(tweetLink);
-    node.type = 'html';
-    node.value = embedData.html;
+    debug(`\nembeding tweet: ${tweetLink}\n`);
+    try {
+      const embedData = await getBloquequote(tweetLink);
+      node.type = 'html';
+      node.value = embedData.html;
+    } catch (er) {
+      debug(`\nfailed to get blockquote for ${tweetLink}\n`, er)
+    }
   }
 
   return markdownAST;
