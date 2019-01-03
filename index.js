0569af07bd17220f80c641e7fa46b3d5b4a85674
isaacs <i@izs.me>
2019-01-03 13:50:22 -0800

    Only embed when twitter link on a line by itself
    Otherwise it does really wierd things to inline links to tweets, which
    looks pretty bad.

--- index.js
+++ index.js
@@ -10,16 +10,26 @@ const getBloquequote = async (url) => {
   return await response.json();
 };
 
+const twitterRegexp = /https:\/\/twitter\.com\/([A-Za-z0-9-_]*\/status\/[A-Za-z0-9-_?=]*)/gi;
+
+// only do the embedding for a single twitter url on its own paragraph.
+const isTwitterLink = node => {
+  return node.children.length === 1 &&
+    node.children[0].type === 'link' &&
+    node.children[0].url.match(twitterRegexp) &&
+    node.children[0].children.length === 1 &&
+    node.children[0].children[0].type === 'text' &&
+    node.children[0].children[0].value === node.children[0].url;
+}
+
 module.exports = async ({ markdownAST }, pluginOptions) => {
   const debug = pluginOptions.debug ? console.log : () => {}
 
   const nodes = [];
-  visit(markdownAST, 'text', (node) => {
-    const { value } = node;
-    const tweetLink = value.match(/https:\/\/twitter\.com\/([A-Za-z0-9-_]*\/status\/[A-Za-z0-9-_?=]*)/gi);
-    if (tweetLink) {
-      debug(`\nfound tweetLink`, node)
-      nodes.push([node, tweetLink]);
+  visit(markdownAST, 'paragraph', (node) => {
+    if (isTwitterLink(node)) {
+      debug(`\nfound tweetLink`, node.children[0].url)
+      nodes.push([node, node.children[0].url])
     }
   })
 
@@ -32,6 +42,7 @@ module.exports = async ({ markdownAST }, pluginOptions) => {
       const embedData = await getBloquequote(tweetLink);
       node.type = 'html';
       node.value = embedData.html;
+      node.children = null;
     } catch (er) {
       debug(`\nfailed to get blockquote for ${tweetLink}\n`, er)
     }
