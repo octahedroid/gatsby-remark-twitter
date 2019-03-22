6169db3167993200eee445f43c099e78b58906c7
isaacs <i@izs.me>
2019-03-22 12:44:13 -0700

    Remove unused babel-cli, update tap to faster v13
    This removes the vulnerable dep on braces, since babel-cli isn't
    actually used directly anywhere in this module anyway.

--- tap-snapshots/test-basic.js-TAP.test.js
+++ tap-snapshots/test-basic.js-TAP.test.js
@@ -6,80 +6,161 @@
  */
 'use strict'
 exports[`test/basic.js TAP basic conversion > should convert twitter link 1`] = `
-{ type: 'root',
-  children:
-   [ { type: 'html',
-       children: null,
-       value:
-        '<blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">' } ] }
+Object {
+  "type": "root",
+  "children": Array [
+    Object {
+      "type": "html",
+      "children": null,
+      "value": "<blockquote class=\\"twitter-tweet\\" data-dnt=\\"true\\"><p lang=\\"en\\" dir=\\"ltr\\">",
+    },
+  ],
+}
 `
 
 exports[`test/basic.js TAP dont convert if nested > should not convert twitter inline link 1`] = `
-{ type: 'root',
-  children:
-   [ { type: 'paragraph',
-       children:
-        [ { type: 'text', value: 'Really, I mean it, like I said in ' },
-          { type: 'link',
-            title: null,
-            url: 'https://twitter.com/izs/status/503226223527337985',
-            children:
-             [ { type: 'text',
-                 value: 'https://twitter.com/izs/status/503226223527337985' } ] },
-          { type: 'text', value: '.' } ] } ] }
+Object {
+  "type": "root",
+  "children": Array [
+    Object {
+      "type": "paragraph",
+      "children": Array [
+        Object {
+          "type": "text",
+          "value": "Really, I mean it, like I said in ",
+        },
+        Object {
+          "type": "link",
+          "title": null,
+          "url": "https://twitter.com/izs/status/503226223527337985",
+          "children": Array [
+            Object {
+              "type": "text",
+              "value": "https://twitter.com/izs/status/503226223527337985",
+            },
+          ],
+        },
+        Object {
+          "type": "text",
+          "value": ".",
+        },
+      ],
+    },
+  ],
+}
 `
 
 exports[`test/basic.js TAP failed conversion > should handle failure nicely 1`] = `
-{ type: 'root',
-  children:
-   [ { type: 'paragraph',
-       children:
-        [ { type: 'link',
-            title: null,
-            url: 'https://twitter.com/izs/status/13425',
-            children:
-             [ { type: 'text', value: 'https://twitter.com/izs/status/13425' } ] } ] } ] }
+Object {
+  "type": "root",
+  "children": Array [
+    Object {
+      "type": "paragraph",
+      "children": Array [
+        Object {
+          "type": "link",
+          "title": null,
+          "url": "https://twitter.com/izs/status/13425",
+          "children": Array [
+            Object {
+              "type": "text",
+              "value": "https://twitter.com/izs/status/13425",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
 `
 
 exports[`test/basic.js TAP failed conversion > should get expected logs 1`] = `
-[ [ '\\nfound tweetLink', 'https://twitter.com/izs/status/13425' ],
-  [ '\\nembeding tweet: https://twitter.com/izs/status/13425\\n' ],
-  [ '\\nfailed to get blockquote for https://twitter.com/izs/status/13425\\n',
-    'ERROR MESSAGE: invalid json response body at https://publish.twitter.com/oembed?url=https://twitter.com/izs/status/13425&hide_thread=1&align=&hide_media=0&theme=&link_color=&widget_type=&omit_script=true&dnt=true&limit=20&chrome=nofooter reason: Unexpected token < in JSON at position 0' ] ]
+Array [
+  Array [
+    "\\nfound tweetLink",
+    "https://twitter.com/izs/status/13425",
+  ],
+  Array [
+    "\\nembeding tweet: https://twitter.com/izs/status/13425\\n",
+  ],
+  Array [
+    "\\nfailed to get blockquote for https://twitter.com/izs/status/13425\\n",
+    "ERROR MESSAGE: invalid json response body at https://publish.twitter.com/oembed?url=https://twitter.com/izs/status/13425&hide_thread=1&align=&hide_media=0&theme=&link_color=&widget_type=&omit_script=true&dnt=true&limit=20&chrome=nofooter reason: Unexpected token < in JSON at position 0",
+  ],
+]
 `
 
 exports[`test/basic.js TAP plugin options, and a moment > an optioned up moment 1`] = `
-{ type: 'root',
-  children:
-   [ { type: 'html',
-       children: null,
-       value:
-        '<a class="twitter-moment" href="https://twitter.com/i/moments/944326645493612545?ref_src=twsrc%5Etfw">The True Meaning of Christmas</a>\\n<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\\n' } ] }
+Object {
+  "type": "root",
+  "children": Array [
+    Object {
+      "type": "html",
+      "children": null,
+      "value": "<a class=\\"twitter-moment\\" href=\\"https://twitter.com/i/moments/944326645493612545?ref_src=twsrc%5Etfw\\">The True Meaning of Christmas</a>\\n<script async src=\\"https://platform.twitter.com/widgets.js\\" charset=\\"utf-8\\"></script>\\n",
+    },
+  ],
+}
 `
 
 exports[`test/basic.js TAP twitter link with two _ chars in username > should convert twitter link 1`] = `
-{ type: 'root',
-  children:
-   [ { type: 'html',
-       children: null,
-       value:
-        '<blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">' } ] }
+Object {
+  "type": "root",
+  "children": Array [
+    Object {
+      "type": "html",
+      "children": null,
+      "value": "<blockquote class=\\"twitter-tweet\\" data-dnt=\\"true\\"><p lang=\\"en\\" dir=\\"ltr\\">",
+    },
+  ],
+}
 `
 
 exports[`test/basic.js TAP twitter link with other random emphasized stuff > should not convert twitter link 1`] = `
-{ type: 'root',
-  children:
-   [ { type: 'paragraph',
-       children:
-        [ { type: 'link',
-            url:
-             'https://twitter.com/Ana_M_Medina/status/1045730232743813121',
-            children:
-             [ { type: 'text', value: 'this makes me feel ' },
-               { type: 'emphasis',
-                 children:
-                  [ { type: 'text', value: 'happy ' },
-                    { type: 'strong', children: [ { type: 'text', value: 'and' } ] },
-                    { type: 'text', value: ' sad' } ] },
-               { type: 'text', value: '.' } ] } ] } ] }
+Object {
+  "type": "root",
+  "children": Array [
+    Object {
+      "type": "paragraph",
+      "children": Array [
+        Object {
+          "type": "link",
+          "url": "https://twitter.com/Ana_M_Medina/status/1045730232743813121",
+          "children": Array [
+            Object {
+              "type": "text",
+              "value": "this makes me feel ",
+            },
+            Object {
+              "type": "emphasis",
+              "children": Array [
+                Object {
+                  "type": "text",
+                  "value": "happy ",
+                },
+                Object {
+                  "type": "strong",
+                  "children": Array [
+                    Object {
+                      "type": "text",
+                      "value": "and",
+                    },
+                  ],
+                },
+                Object {
+                  "type": "text",
+                  "value": " sad",
+                },
+              ],
+            },
+            Object {
+              "type": "text",
+              "value": ".",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
 `
